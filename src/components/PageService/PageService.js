63415506228d6cb1d66ca9991d27ac25a32ec77a
rochyscarlata <rosarioscarlata24@gmail.com>
2020-10-11 17:06:37 -0300

    solucionado contacto

--- src/components/PageService/PageService.js
+++ src/components/PageService/PageService.js
@@ -24,23 +24,7 @@ const PageService = ({ authen, setAuthen, usuario }) => {
           <div className="w-100 d-flex justify-content-center mt-5">
           <Image fluid src={Loguito} style={{width:"150px"}}/>
           </div>
-          <Row className="d-flex flex-column justify-content-center p-5">
-            <Col>
-              <h1 className="text-center textos text-justify">QUIENES SOMOS</h1>
-            </Col>
-            <Col className="p-3 text-center font-italic">
-              <h4 className="font-weight-light">ZeroTech - 2020 - Arg</h4>
-            </Col>
-            <Col>
-              <p className="p-3 text-center text-justify">
-                Zerotech posee un servicio técnico donde se realizan
-                reparaciones de productos electrónicos de todas las
-                marcas,tenemos técnicos especializados. Trabajamos con repuestos
-                originales y entregamos garantías escritas. Financiamos con
-                tarjetas, Recibimos débito y crédito.
-              </p>
-            </Col>
-          </Row>
+         
 
           <Row className="d-flex flex-column justify-content-center p-5 mt-3">
             <Col>
@@ -48,13 +32,14 @@ const PageService = ({ authen, setAuthen, usuario }) => {
             </Col>
             <Col className="p-3 text-center font-italic">
               <h4 className="font-weight-light">
-                SOMOS SERVICIO OFICIAL de las principales marcas de audio y
-                video.
+                Monteagudo 747
+                <br/>
+                <i class="fas fa-phone-alt m-2"></i>3814474779
               </h4>
             </Col>
             
-            <Col className="p-2">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.5795297969553!2d-65.19986228529636!3d-26.821513795866068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225c18b83c26c5%3A0x53d23cc8bc68f9e!2sBernardo%20de%20Monteagudo%20747%2C%20T4000%20San%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1599968230832!5m2!1ses!2sar" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <Col className="m-auto">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.5795297969553!2d-65.19986228529636!3d-26.821513795866068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225c18b83c26c5%3A0x53d23cc8bc68f9e!2sBernardo%20de%20Monteagudo%20747%2C%20T4000%20San%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1599968230832!5m2!1ses!2sar"  height="450" frameborder="0" style={{border:0}, {width:"100%"} } allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </Col>
           </Row>
 
